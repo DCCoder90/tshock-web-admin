171ff4c9e39869eae92ac0759c7a549f60429b6e
Darkvengance aka Sildaekar <admin@rejectedfreaks.net>
2012-02-06 23:43:05 -0600

    Fixed bug in serverstatus image

--- images/serverstatus.php
+++ images/serverstatus.php
@@ -1,5 +1,6 @@
 <?php
-include("../inc/db.php");
+chdir("../");
+include("./inc/db.php");
 
 //Creates the server status image
 $name=urldecode($_GET['name']);
@@ -7,24 +8,32 @@ $playercount=(int)$_GET['playercount'];
 $port=(int)$_GET['port'];
 $status=(int)$_GET['status'];
 
-$img=ImageCreateFromJPEG(HOME_URL."/images/status.jpg");
-$color = imagecolorallocate($img, 0, 0, 0);
+if($status==200){
+	$image="Online.jpg";
+}else{
+	$image="Offline.jpg";
+}
+
+$img=ImageCreateFromJPEG(HOME_URL."/images/".$image);
+
 
 
 if($status==200){
 	$statustxt="Up";
-	$status_color=imagecolorallocate($img,0,255,0);
+	$color = imagecolorallocate($img, 0, 0, 0);
+	$status_color=imagecolorallocate($img,255,255,0);
 }elseif($status==500){
 	$statustxt="Down";
+	$color = imagecolorallocate($img, 255, 0, 0);
 	$status_color=imagecolorallocate($img,255,0,0);
 }else{
 	$statustxt="Error";
 	$status_color=imagecolorallocate($img,0,255,255);
 }
 
-imagestring( $img,5,20,22,"Map Name: ".$name,$color); //name
-imagestring( $img,5,20,42,"Port: ".$port,$color); //port
-imagestring( $img,5,20,52,"Status: ".$statustxt,$status_color); //status
+imagestring( $img,5,20,2,"Map Name: ".$name,$color); //name
+imagestring( $img,5,20,12,"Port: ".$port,$color); //port
+imagestring( $img,5,20,22,"Status: ".$statustxt,$status_color); //status
 
 if($status!=500){
 	imagestring( $img,5,20,32,"Players: ".$playercount,$color); //count
