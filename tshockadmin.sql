e8ab1dd36a362a50bdf5c61128ec9188da9e5079
Darkvengance aka Sildaekar <admin@rejectedfreaks.net>
2012-02-06 21:50:51 -0600

    Updated SQL file

--- tshockadmin.sql
+++ tshockadmin.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Feb 02, 2012 at 06:09 AM
+-- Generation Time: Feb 07, 2012 at 04:50 AM
 -- Server version: 5.5.16
 -- PHP Version: 5.3.8
 
@@ -58,11 +58,14 @@ CREATE TABLE IF NOT EXISTS `servers` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `ip` varchar(15) NOT NULL,
   `port` int(5) NOT NULL,
+  `restport` int(5) NOT NULL,
+  `user_name` varchar(255) NOT NULL,
+  `user_pass` varchar(255) NOT NULL,
   `name` varchar(75) NOT NULL,
   `log_type` int(1) NOT NULL DEFAULT '0' COMMENT '0=Local log 1=Remote log via FTP',
   `log_name` varchar(255) NOT NULL COMMENT 'Log file name',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 -- --------------------------------------------------------
 
@@ -80,6 +83,7 @@ CREATE TABLE IF NOT EXISTS `server_logs` (
   `log_action` varchar(255) NOT NULL,
   `log_info` tinytext NOT NULL,
   `log_misc` tinytext NOT NULL,
+  `priority` int(1) NOT NULL,
   `date` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
@@ -97,7 +101,20 @@ CREATE TABLE IF NOT EXISTS `settings` (
   `meta_desc` tinytext NOT NULL,
   `meta_keywords` tinytext NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `site_sessions`
+--
+
+CREATE TABLE IF NOT EXISTS `site_sessions` (
+  `session_id` int(11) NOT NULL,
+  `session_data` text NOT NULL,
+  `session_expires` int(11) NOT NULL,
+  UNIQUE KEY `session_id` (`session_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -111,7 +128,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `user_email` varchar(255) NOT NULL,
   `user_pass` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 -- --------------------------------------------------------
 
