60b3e654d70d661d0a1a2d4f19f103b7066a5ddb
Darkvengance aka Sildaekar <admin@rejectedfreaks.net>
2012-02-06 21:35:53 -0600

    Added command_player files

---
+++ command_player.php
@@ -0,0 +1,72 @@
+<?php
+require('inc/db.php');
+check_loggedin();
+
+if(isset($_GET['cmd'])){
+	$cmd=$_GET['cmd'];
+	$user=(!isset($_GET['usr']))?$_POST['usr']:$_GET['usr'];
+	$reason=(!isset($_GET['rsn']))?null:$_GET['rsn'];
+
+	$sid=(!isset($_GET['sid']))?(int)$_POST['sid']:(int)$_GET['sid'];
+
+	$result=$db->query("SELECT * FROM `servers` WHERE `id`=$sid LIMIT 1");
+	$server=$result->fetch_assoc();
+	$result->close();
+
+	$rest->set_server($server['ip'],$server['port'],null,$server['user_name'],$server['user_pass']);
+
+	switch($cmd){
+		case "read":
+			$parse=new parser;
+			$resp=$rest->player_read($user);
+
+			$inventory=$parse->parse_inventory($resp['inventory']);
+			$pos=explode(",",$resp['position']);
+			$response="Nickname: ".$resp['nickname']."<br />
+						Username: ".$resp['username']."<br />
+						IP: ".$resp['ip']."<br />
+						Group: ".$resp['group']."<br />
+						Position: X-".$pos[0]."  Y-".$pos[1]."<br />
+						Inventory: <br />";
+			foreach($inventory as $item){
+				$response=$response.$item['name']."-".$item['amount']."<br />";
+			}
+			$response=$response."Buffs:<br />".$resp['buffs'];
+		break;
+
+		case "kick":
+			$resp=$rest->player_kick($user,$reason);
+			$response=$user." kicked!";
+		break;
+
+		case "ban":
+			$resp=$rest->player_ban($user,$reason);
+			$response=$user." banned!";
+		break;
+
+		case "kill":
+			$resp=$rest->player_kill($user);
+			$response=$user." was killed!";
+		break;
+
+		case "mute":
+			$resp=$rest->player_mute($user,$reason);
+			$response=$user." muted!";
+		break;
+
+		case "unmute":
+			$resp=$rest->player_unmute($user,$reason);
+			$response=$user." unmuted!";
+		break;
+
+
+		default:
+			die("Command not specified");
+		break;
+
+	}
+	$smarty->assign("response",$response);
+}
+
+$smarty->display('command_player.tpl');
+?>
---
+++ temp/templates/command_player.tpl
@@ -0,0 +1,25 @@
+{include file='inc/header.tpl'}
+
+<form action="{$url}/command_user.php" method="get">
+<input type="hidden" name="sid" value="">
+
+Player: <input type="text" name="usr" /><br />
+Action: <select name="cmd">
+			<option value="read">Read</option>
+			<option value="kick">Kick</option>
+			<option value="ban">Ban</option>
+			<option value="kill">Kill</option>
+			<option value="mute">Mute</option>
+			<option value="unmute">Unmute</option>
+		</select><br />
+
+<input type="submit" value="Execute!">
+</form>
+<br /><br />
+
+{if $response neq ""}
+	<div id="response">
+		{$response}
+	</div>
+{/if}
+{include file='inc/footer.tpl'}
